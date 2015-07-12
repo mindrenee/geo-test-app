d325ceb8f031ba44653b06a57475940fb6178208
mindrenee <reneewitsenburg@live.nl>
2015-07-12 17:18:01 +0200

    extra alert

--- js/geolocatie.js
+++ js/geolocatie.js
@@ -30,6 +30,15 @@ function deviceReady() {
 }// end deviceReady();
 
 function onSuccess(position) {
+	alert('Latitude: '          + position.coords.latitude          + '\n' +
+          'Longitude: '         + position.coords.longitude         + '\n' +
+          'Altitude: '          + position.coords.altitude          + '\n' +
+          'Accuracy: '          + position.coords.accuracy          + '\n' +
+          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+          'Heading: '           + position.coords.heading           + '\n' +
+          'Speed: '             + position.coords.speed             + '\n' +
+          'Timestamp: '         + position.timestamp                + '\n');
+	
 	setCurrentPosition(position.coords.latitude, position.coords.longitude);
 }
 
